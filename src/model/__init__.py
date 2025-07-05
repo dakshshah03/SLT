10721eaa67242c20f542749a2d100b00e4a6099a
Daksh Shah <daksh.shah15@gmail.com>
2025-07-05 16:39:43 -0700

    Updated directory format

---
+++ src/model/__init__.py
@@ -0,0 +1,1 @@
+from video_mae import get_videoMAE_ssv2
--- src/model/video_mae.py
+++ src/model/video_mae.py
@@ -1,6 +1,11 @@
-# import torch
 from transformers import VideoMAEForVideoClassification
+import torch
 
-model = VideoMAEForVideoClassification.from_pretrained("MCG-NJU/videomae-base-finetuned-kinetics")
+model = VideoMAEForVideoClassification.from_pretrained("")
 
-print(model)
+def get_videoMAE_ssv2(num_classes):
+    model = VideoMAEForVideoClassification.from_pretrained(
+        "MCG-NJU/videomae-base-finetuned-ssv2"
+    )
+    model.classifier = torch.nn.Linear(model.config.hidden_size, num_classes)
+    return model
