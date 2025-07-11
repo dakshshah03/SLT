1b23597b8722eef98aca0f05cb6ca4feb4cab458
Daksh Shah <daksh.shah15@gmail.com>
2025-07-11 14:14:33 -0700

    train job config

--- src/train.py
+++ src/train.py
@@ -6,28 +6,44 @@ from utils import asl_citizen_dataset
 
 import mlflow
 
-
-training_args = TrainingArguments(
-    output_dir="./model_checkpoints",
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=20,
-    evaluation_strategy="epoch",
-    logging_strategy="steps",
-    logging_steps=50,
-    save_strategy="epoch",
-    report_to="mlflow",
-    run_name="videomae-asl-run",
-)
+import argparse
 
 
-
-
-trainer_classifier = Trainer(
-    model=get_videoMAE_ssv2,
-    args=training_args,
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+       "Training script to finetune VideoMAE-ssv2 for ASL Sign Language Translation",
+        add_help=False
+    )
     
-)
-
-# if __name__=="__main__":
+    # TODO: Add more arguments so parameters are less of a pain to set up
+    parser.add_argument('--train_batch_size', default=32, type=int)
+    parser.add_argument('--eval_batch_size', default=32, type=int)
+    parser.add_argument('--epochs', default=20, type=int)
+    parser.add_argument('--output_dir', default="./model_artifacts", type=str)
+    parser.add_argument('--report_to', default="mlflow", type=str)
     
+    args = parser.parse_args()
+    
+    
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        per_device_train_batch_size=args.train_batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        num_train_epochs=args.epochs,
+        evaluation_strategy="epoch",
+        logging_strategy="steps",
+        logging_steps=50,
+        save_strategy="epoch",
+        report_to=args.report_to,
+        run_name="videomae-asl-run",
+    )
+    
+    model = get_videoMAE_ssv2(200)
+    optimizer = get_videoMAE_adamW(model)
+    
+    # TODO: finish training initialization
+    trainer_classifier = Trainer(
+        model=model,
+        args=training_args,
+    )
+    # TODO: MLFlow Logging
